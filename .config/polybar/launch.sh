eb02b1f943532b113bed4386da84a30b1afe7cb5
Adam Collins <adc613@gmail.com>
2025-07-30 10:13:28 -0500

    Create systemd service for polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,7 +1,8 @@
+#!/bin/sh
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload --config=$HOME/.config/polybar/config.ini example &
+    MONITOR=$m polybar --reload --config=/home/adam/.config/polybar/config.ini example &
   done
 else
-  polybar --reload --config=$HOME/.config/polybar/config.ini example &
+  polybar --reload --config=/home/adam/.config/polybar/config.ini example &
 fi
